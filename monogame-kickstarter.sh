377a00bb2f4ef113e2fe8e19b85c66aaecfb30cc
Kwyrky3 <kwyrky@gmail.com>
2020-09-01 00:43:57 +0200

    command line parameters wip

--- monogame-kickstarter.sh
+++ monogame-kickstarter.sh
@@ -1,6 +1,131 @@
 #! /bin/bash
 
-version="1.0"
+version="1.01"
+
+################################################################################
+# COMMAND LINE OPTIONS
+################################################################################
+
+# saner programming env: these switches turn some bugs into errors
+#set -o errexit -o pipefail -o noclobber -o nounset
+
+# -allow a command to fail with !’s side effect on errexit
+# -use return value from ${PIPESTATUS[0]}, because ! hosed $?
+! getopt --test > /dev/null 
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment.'
+    exit 1
+fi
+
+OPTIONS=n:dsvaoiuxw
+LONGOPTS=name:,debug,solution,verbose,mgandroid,mgdesktopgl,mgios,mguwpcore,mguwpxaml,mgwindowsdx
+
+# -regarding ! and PIPESTATUS see above
+# -temporarily store output to be able to check for errors
+# -activate quoting/enhanced mode (e.g. by writing out “--options”)
+# -pass arguments only via   -- "$@"   to separate them correctly
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    # e.g. return value is 1
+    #  then getopt has complained about wrong arguments to stdout
+    exit 2
+fi
+# read getopt’s output this way to handle the quoting right:
+eval set -- "$PARSED"
+
+# set defaults
+n=monogamekickstarter
+#
+d=y
+s=y
+v=y
+#
+a=n
+o=y
+i=n
+u=n
+x=n
+w=n
+
+
+# now enjoy the options in order and nicely split until we see --
+while true; do
+    case "$1" in
+        -n|--name)
+            name="$2"
+            shift 2
+            ;;    
+        -d|--debug)
+            d=y
+            shift
+            ;;
+        -s|--solution)
+            s=y
+            shift
+            ;;
+        -v|--verbose)
+            v=y
+            shift
+            ;;
+        -a|--mgandroid)
+            a=y
+            shift
+            ;;
+        -o|--mgdesktopgl)
+            o=y
+            shift
+            ;;
+        -i|--mgios)
+            i=y
+            shift
+            ;;
+        -u|--mguwpcore)
+            u=y
+            shift
+            ;;
+        -x|--mguwpxaml)
+            x=y
+            shift
+            ;;
+        -w|--mgwindowsdx)
+            w=y
+            shift
+            ;;                                                
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
+
+# handle non-option arguments
+#if [[ $# -ne 1 ]]; then
+#    echo "$0: A single input file is required."
+#    exit 4
+#fi
+
+if [ $d == y ]; then
+echo "name: $n"
+#
+echo "debug: $d"
+echo "solution: $s"
+echo "verbose: $v"
+#
+echo "mgandroid: $a"
+echo "mgdesktopgl: $o"
+echo "mgios: $i"
+echo "mguwpcore: $u"
+echo "mguwpxaml: $x"
+echo "mgwindowsdx: $w"
+fi
+
+################################################################################
+# COMMAND LINE OPTIONS
+################################################################################
 
 # constants
 delimiter="################################################################################"
